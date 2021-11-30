80091cb0a3ddc9998d9eaaa38eda447b27a3e528
Евгений Силенков <ensilenkov@gmail.com>
2021-11-30 23:38:44 +0700

    Добавьте файлы проекта.

---
+++ Lab5.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        array[i, j] = 1;
+                    }
+                    else
+                    {
+                        array[i, j] = 0;
+                    }
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int [7];
+            int S = 0;
+            int R;
+            for (int i = 0; i < 7; i++)
+            {
+                array [i] = Convert.ToInt32(Console.ReadLine());
+                S = S + array [i];
+            }
+            R = S / 7;
+            Console.WriteLine(R);
+            Console.ReadKey();
+        }
+    }
+}
